337b00e25240d68500181b0bedca285641488e01
Ignacio Iacovino <nacho.iacovino@gmail.com>
2019-11-11 17:40:24 +0100

    Style ColorBox with Flex

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,10 +3,19 @@ import './ColorBox.css'
 
 export default class ColorBox extends Component {
     render() {
+        const { name, background } = this.props
         return (
-            <div style={{background: this.props.background}} className="ColorBox">
-                <span>{this.props.name}</span>
-                <span>MORE</span>
+            <div style={{ background }} className="ColorBox">
+                <div className="copy-container">
+                    <div className="p10">&nbsp;</div>
+                    <button className="copy-button">
+                        Copy
+                    </button>
+                    <div className="box-content">
+                        <span className="p10">{name}</span>
+                        <span className="see-more">More</span>
+                    </div>
+                </div>
             </div>
         )
     }
